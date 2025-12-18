feea99c21e056716d1baf1a5ef95ac657432d2b9
bekicom <adelkauz6@gmail.com>
2025-12-18 07:57:20 +0500

    start

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ const router = express.Router();
 // Controllers
 const authController = require("../controllers/auth.controller");
 const userController = require("../controllers/user.controller");
+const warehouseController = require("../controllers/warehouse.controller");
+
 
 // Middlewares
 const { rAuth, rRole } = require("../middlewares/auth.middleware");
@@ -22,6 +24,17 @@ router.get("/users", rAuth, rRole("ADMIN"), userController.getUsers);
 router.get("/users/:id", rAuth, rRole("ADMIN"), userController.getUserById);
 router.put("/users/:id", rAuth, rRole("ADMIN"), userController.updateUser);
 router.delete("/users/:id", rAuth, rRole("ADMIN"), userController.deleteUser);
+
+
+/**
+ * WAREHOUSES (ADMIN only)
+ */
+router.post("/warehouses/create", rAuth, rRole("ADMIN"), warehouseController.createWarehouse);
+router.get("/warehouses", rAuth, rRole("ADMIN"), warehouseController.getWarehouses);
+router.get("/warehouses/:id", rAuth, rRole("ADMIN"), warehouseController.getWarehouseById);
+router.put("/warehouses/:id", rAuth, rRole("ADMIN"), warehouseController.updateWarehouse);
+router.delete("/warehouses/:id", rAuth, rRole("ADMIN"), warehouseController.deleteWarehouse);
+
 
 
 module.exports = router;
